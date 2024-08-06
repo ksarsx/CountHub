b8e6ab41b57abf855a313deaa8079b014daa3421
Ksar Sx <egorvereykin2@gmail.com>
2024-08-06 23:58:18 +0300

    v2

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,5 +1,19 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class Controller {
+	
+	/*
+	 * title buttons
+	 */
+	
+	
+	
+	/*
+	 * finish of title buttons
+	 */
 	
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -17,6 +17,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			stage.initStyle(StageStyle.DECORATED.UNDECORATED);
+			
 			stage.setScene(scene);
 			stage.show();
 		} catch(Exception e) {
