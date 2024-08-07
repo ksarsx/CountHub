0cbf436feb164053869a72d66ca492fd91b2ff2f
Ksar Sx <egorvereykin2@gmail.com>
2024-08-08 02:21:53 +0300

    v3

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,8 +1,11 @@
 package application;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class Controller {
 	
@@ -10,7 +13,22 @@ public class Controller {
 	 * title buttons
 	 */
 	
+	@FXML
+	private void btnCloseReleased(MouseEvent event) {
+		Node source = (Node) event.getSource();
+		Stage stage = (Stage) source.getScene().getWindow();
+		stage.close();
+	}
 	
+	@FXML
+	private void btnFullScreenReleased(MouseEvent event) {
+		System.out.println("````");
+	}
+	
+	@FXML
+	private void btnHideReleased(MouseEvent event) {
+		System.out.println("````");
+	}
 	
 	/*
 	 * finish of title buttons
