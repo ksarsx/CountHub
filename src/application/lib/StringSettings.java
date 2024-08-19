0ba74becfa80271d948166cb62381ed191195abf
Ksar Sx <egorvereykin2@gmail.com>
2024-08-19 23:18:49 +0300

    p3

--- src/application/lib/StringSettings.java
+++ src/application/lib/StringSettings.java
@@ -1,6 +1,7 @@
 package application.lib;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class StringSettings {
 	
@@ -18,6 +19,37 @@ public class StringSettings {
             }
         }
         return text;
+    }
+	
+	public static String[] generate_equation(int originFirstNum, int boundFirstNum, int originSecondNum, int boundSecondNum, String typeOfOperation, int lastNumber1, int lastNumber2) {
+        Random rand = new Random();
+        int num1 = rand.nextInt(originFirstNum, boundFirstNum);
+        int num2 = rand.nextInt(originSecondNum, boundSecondNum);
+        while ((num1 == lastNumber1) || (num2 == lastNumber2)) {
+            if (num1 == lastNumber1) {
+                num1 = rand.nextInt(originFirstNum, boundFirstNum);
+            }
+            if (num2 == lastNumber2) {
+                num2 = rand.nextInt(originSecondNum, boundSecondNum);
+            }
+        }
+        double result = 0;
+        switch (typeOfOperation) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                result = (double) num1 / (double) num2;
+        }
+
+        String[] arr = {String.valueOf(num1) + " " + typeOfOperation + " " +  String.valueOf(num2), String.valueOf(result)};
+        return arr;
     }
 	
 }
